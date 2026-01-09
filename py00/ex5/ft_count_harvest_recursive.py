fae4cbb3b244c0fea736f520d58648f9b6856c58
Lucas Bordanave <lbordana@k1r3p2.42mulhouse.fr>
2026-01-09 17:05:56 +0100

    feat: added ex6 and ex7

--- py00/ex5/ft_count_harvest_recursive.py
+++ py00/ex5/ft_count_harvest_recursive.py
@@ -1,6 +1,8 @@
-def ft_count_harvest_recursive(days=int(input("Days until harvest: "))):
-    if days > 1:
+def ft_count_harvest_recursive(days=None):
+    if days is None:
+        days = int(input("Days until harvest: "))
+        ft_count_harvest_recursive(days)
+        print("Harvest time!")
+    elif days > 0:
         ft_count_harvest_recursive(days - 1)
-        print("Days", days)
-    else:
-        print("Time to harvest!")
+        print("Day", days)
