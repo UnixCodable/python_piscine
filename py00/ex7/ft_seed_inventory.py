4b7739648ffbb3aa5cb28227a3479c59b2244b7d
Lucas Bordanave <lbordana@k1r3p2.42mulhouse.fr>
2026-01-09 17:09:47 +0100

    feat: added ex6 and ex7

--- py00/ex7/ft_seed_inventory.py
+++ py00/ex7/ft_seed_inventory.py
@@ -1,12 +1,10 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    seed_type = seed_type.capitalize()
     if unit == "packets":
-        unit = print(f"{seed_type.capitalize()} seeds: \
-{quantity} packets available")
+        print(f"{seed_type} seeds: {quantity} packets available")
     elif unit == "grams":
-        unit = print(f"{seed_type.capitalize()} seeds: \
-{quantity} grams total")
+        print(f"{seed_type} seeds: {quantity} grams total")
     elif unit == "area":
-        unit = print(f"{seed_type.capitalize()} seeds: covers \
-{quantity} square meters")
+        print(f"{seed_type} seeds: covers {quantity} square meters")
     else:
         print("Unknown unit type")
