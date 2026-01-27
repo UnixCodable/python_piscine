dad171f5111351aa4e3521ccb7f9787bf084062f
Lucas Bordanave <lbordana@k1r3p1.42mulhouse.fr>
2026-01-27 17:46:47 +0100

    feat: added py00/ex4

---
+++ py01/ex6/ft_garden_analytics.py
@@ -0,0 +1,21 @@
+class GardenManager:
+    def __init__():
+        pass
+
+    def GardenStats():
+        pass
+
+    def create_garden_network():
+        pass
+
+
+class Plant:
+    pass
+
+
+class FloweringPlant:
+    pass
+
+
+class PrizeFlower:
+    pass
