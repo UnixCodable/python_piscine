5c5cfefc0321470bae96cf5762cd2aef7ae9b651
Lucas Bordanave <lbordana@k1r3p1.42mulhouse.fr>
2026-01-27 13:55:38 +0100

    feat: added py01/ex5

--- py01/ex4/ft_garden_security.py
+++ py01/ex4/ft_garden_security.py
@@ -10,10 +10,10 @@ class SecurePlant:
         self.__height = 0
         self.__age_value = 0
         print(f"Plant created: {p_name}")
-    
+
     def set_height(self, p_height: int):
         """Setter made to encapsulate access to height data. Update an height.
-        
+
         Args:
             p_height (int): The new height of the plant
         """
@@ -24,10 +24,10 @@ class SecurePlant:
             print(f"""\
 Invalid operation attempted: height {p_height}cm [REJECTED]
 Security: Negative height rejected""")
-    
+
     def set_age(self, p_age: int):
         """Setter made to encapsulate access to age data. Update an age.
-        
+
         Args:
             p_age (int): The new age of the plant
         """
@@ -38,14 +38,15 @@ Security: Negative height rejected""")
             print(f"""\
 Invalid operation attempted: age {p_age} days [REJECTED]
 Security: Negative age rejected""")
-    
+
     def get_age(self):
         """Getter made to securely retrieve plant age"""
         return self.__age_value
-    
+
     def get_height(self):
         """Getter made to securely retrieve plant height"""
         return self.__height
+
 
 print("=== Garden Security System ===")
 rose = SecurePlant("Rose")
--- py01/ex5/ft_plant_types.py
+++ py01/ex5/ft_plant_types.py
@@ -1,56 +1,64 @@
-class SecurePlant:
-    """A secure class defining a plant"""
-    def __init__(self, p_name: str):
+class Plant:
+    """A class defining a plant"""
+    def __init__(self, p_name: str, p_height: int, p_age: int):
         """Initialize plant with their identity
 
         Args:
             p_name (string): The name of the plant
+            p_height (int): The height of the plant in centimeters
+            p_age (int): The age of the plant
         """
         self.name = p_name
-        self.__height = 0
-        self.__age_value = 0
-        print(f"Plant created: {p_name}")
-    
-    def set_height(self, p_height: int):
-        """Setter made to encapsulate access to height data. Update an height.
-        
-        Args:
-            p_height (int): The new height of the plant
-        """
-        if p_height >= 0:
-            self.__height = p_height
-            print(f"Height updated: {p_height} days [OK]")
-        else:
-            print(f"""\
-Invalid operation attempted: height {p_height}cm [REJECTED]
-Security: Negative height rejected""")
-    
-    def set_age(self, p_age: int):
-        """Setter made to encapsulate access to age data. Update an age.
-        
-        Args:
-            p_age (int): The new age of the plant
-        """
-        if p_age >= 0:
-            self.__age_value = p_age
-            print(f"Age updated: {p_age} days [OK]")
-        else:
-            print(f"""\
-Invalid operation attempted: age {p_age} days [REJECTED]
-Security: Negative age rejected""")
-    
-    def get_age(self):
-        """Getter made to securely retrieve plant age"""
-        return self.__age_value
-    
-    def get_height(self):
-        """Getter made to securely retrieve plant height"""
-        return self.__height
+        self.height = p_height
+        self.age_value = p_age
 
-print("=== Garden Security System ===")
-rose = SecurePlant("Rose")
-rose.set_height(25)
-rose.set_age(30)
 
-print(f"\nCurrent plant: {rose.name} ({rose.get_height()}cm,\
-{rose.get_age()} days)")
+class Tree(Plant):
+    def __init__(self, p_name: str, p_height: int, p_age: int,
+                 p_trunk_diameter: int):
+        super().__init__(p_name, p_height, p_age)
+        self.trunk_diameter = p_trunk_diameter
+
+    def produce_shade(self):
+        print(f"{self.name} provides {self.trunk_diameter * 2}\
+square meters of shade\n")
+
+
+class Flower(Plant):
+    def __init__(self, p_name: str, p_height: int, p_age: int, p_color: str):
+        super().__init__(p_name, p_height, p_age)
+        self.color = p_color
+
+    def bloom(self):
+        print(f"{self.name} is blooming beautifully !\n")
+
+
+class Vegetable(Plant):
+    def __init__(self, p_name: str, p_height: int,
+                 p_age: int, p_harvest_season: str, p_nutritional_value: str):
+        super().__init__(p_name, p_height, p_age)
+        self.harvest_season = p_harvest_season
+        self.nutritional_value = p_nutritional_value
+
+
+plants = [Tree("Oak", 500, 1825, 50),
+          Tree("Pine", 1200, 350, 60),
+          Flower("Rose", 25, 30, "red"),
+          Flower("Sunflower", 40, 15, "yellow"),
+          Vegetable("Tomato", 80, 90, "summer harvest", "vitamin C"),
+          Vegetable("Pumpkin", 40, 45, "autumn harvest", "beta carotene")]
+
+print("=== Garden Plant Type ===\n")
+for plant in plants:
+    if type(plant) is Tree:
+        print(f"{plant.name} (Tree): {plant.height}cm, {plant.age_value} days,\
+{plant.trunk_diameter}cm diameter")
+        plant.produce_shade()
+    if type(plant) is Flower:
+        print(f"{plant.name} (Flower): {plant.height}cm, {plant.age_value}\
+days, {plant.color} color")
+        plant.bloom()
+    if type(plant) is Vegetable:
+        print(f"{plant.name} (Vegetable): {plant.height}cm, {plant.age_value}\
+days, {plant.harvest_season}")
+        print(f"{plant.name} is rich in {plant.nutritional_value}\n")
