4fa331871551df878d366a5f0e5c55a70e3d524c
Lucas Bordanave <lbordana@k1r3p1.42mulhouse.fr>
2026-01-28 17:45:06 +0100

    feat: added py00/ex4

--- py01/ex6/ft_garden_analytics.py
+++ py01/ex6/ft_garden_analytics.py
@@ -1,21 +1,44 @@
 class GardenManager:
-    def __init__():
+    class GardenStats:
+        def __init__():
+            pass
         pass
 
-    def GardenStats():
-        pass
+    def __init__(self, p_owner: str):
+        self.garden_owner = p_owner
 
     def create_garden_network():
         pass
 
 
-class Plant:
+class Plant(GardenManager):
+    def __init__(self, p_name: str, p_height: int, p_age: int, p_owner: str):
+        """Initialize plant with their identity
+
+        Args:
+            p_name (string): The name of the plant
+            p_height (int): The height of the plant in centimeters
+            p_age (int): The age of the plant
+        """
+        super().__init__(p_owner)
+        self.name = p_name
+        self.height = p_height
+        self.age = p_age
     pass
 
 
-class FloweringPlant:
+class FloweringPlant(Plant):
+    def __init__():
+        pass
     pass
 
 
-class PrizeFlower:
+class PrizeFlower(Plant):
+    def __init__():
+        pass
     pass
+
+
+rose = Plant('Rose', 10, 5, 'Alice')
+
+print(f"{rose.garden_owner}")
