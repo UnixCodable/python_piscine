2eb65cf48349f4ed8a025d4b14df36b15aa79600
UnixCodable <lucasbordanave@gmail.com>
2026-02-02 03:46:58 +0100

    feat: added py01/ex6

--- py01/ex6/ft_garden_analytics.py
+++ py01/ex6/ft_garden_analytics.py
@@ -1,21 +1,43 @@
 class GardenManager:
-    def __init__():
+    class GardenStats:
+        managed_garden = 0
+        regular_plants = 0
+        flowering_plant = 0
+        prize_plant = 0
         pass
 
-    def GardenStats():
-        pass
+    def __init__(self, p_owner: str):
+        self.owner = p_owner
 
-    def create_garden_network():
-        pass
+    @staticmethod
+    def create_garden_network(garden_list: list):
+        network = []
+        for garden in garden_list:
+            network.append(GardenManager(garden))
+        return network
+
+class Plant(GardenManager):
+    """A class defining a plant"""
+    def __init__(self, p_name: str, p_height: int, p_owner: str):
+        """Initialize plant with their identity
+
+        Args:
+            p_name (string): The name of the plant
+            p_height (int): The height of the plant in centimeters
+            p_age (int): The age of the plant
+        """
+        super().__init__(p_owner)
+        self.name = p_name
+        self.height = p_height
 
 
-class Plant:
-    pass
+class FloweringPlant(Plant):
+    def __init__(self, p_name: str, p_height: int, p_owner: str, p_color: str):
+        super().__init__(p_name, p_height, p_owner)
+        self.color = p_color
 
 
-class FloweringPlant:
-    pass
-
-
-class PrizeFlower:
-    pass
+class PrizeFlower(FloweringPlant):
+    def __init__(self, p_name: str, p_height: int, p_owner: str, p_color: str):
+        super().__init__(self, p_name, p_height, p_owner, p_color)
+        GardenManager.GardenStats
