fdf90e29cace5e2d7fbc895bdd0350691bb02f39
Lucas Bordanave <lbordana@k1r3p1.42mulhouse.fr>
2026-02-11 17:15:02 +0100

    feat: added py02/ex3 and ex4

---
+++ py02/ex5/ft_garden_management.py
@@ -0,0 +1,26 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_garden_management.py                           :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/02/11 16:50:37 by lbordana        #+#    #+#               #
+#  Updated: 2026/02/11 17:14:53 by lbordana        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+class GardenManager():
+    plants = {}
+
+    @classmethod
+    def add_plant(cls, p_name):
+        pass
+
+    @classmethod
+    def water_plants(cls, p_name):
+        pass
+
+    @classmethod
+    def check_plant_health(cls, p_name):
+        pass
