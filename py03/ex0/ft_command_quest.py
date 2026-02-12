121e529f0474d85eb93576f6d16bbcf52dabc8cb
UnixCodable <lucasbordanave@gmail.com>
2026-02-12 21:05:26 +0100

    feat: added py03/ex1

---
+++ py03/ex0/ft_command_quest.py
@@ -0,0 +1,27 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_command_quest.py                               :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/02/12 19:55:35 by lbordana        #+#    #+#               #
+#  Updated: 2026/02/12 20:41:43 by lbordana        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+import sys
+
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
+        print('No arguments provided!')
+    print(f'Program name : {sys.argv[0]}')
+    if len(arguments) > 0:
+        i = 0
+        print(f'Arguments received : {len(arguments)}')
+        for arg in arguments:
+            i += 1
+            print(f'Argument {i}: {arg}')
+    print(f'Total arguments: {len(arguments) + 1}')
+    
