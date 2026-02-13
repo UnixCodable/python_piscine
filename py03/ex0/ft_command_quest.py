bafdcf75d1f455474cdbad870bbb9bd16b16f821
Lucas Bordanave <lbordana@k1r3p1.42mulhouse.fr>
2026-02-13 12:29:19 +0100

    feat: added py03/ex0

--- py03/ex0/ft_command_quest.py
+++ py03/ex0/ft_command_quest.py
@@ -5,8 +5,26 @@
 #                                                  +:+ +:+         +:+      #
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
-#  Created: 2026/02/12 17:15:47 by lbordana        #+#    #+#               #
-#  Updated: 2026/02/13 12:28:03 by lbordana        ###   ########.fr        #
+#  Created: 2026/02/12 19:55:35 by lbordana        #+#    #+#               #
+#  Updated: 2026/02/13 12:29:10 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+import sys
+
+def command():
+    arguments = sys.argv[1:]
+    print('=== Command Quest ===')
+    if len(arguments) == 0:
+        print('No arguments provided!')
+    print(f'Program name : {sys.argv[0]}')
+    if len(arguments) > 0:
+        i = 0
+        print(f'Arguments received : {len(arguments)}')
+        for arg in arguments:
+            i += 1
+            print(f'Argument {i}: {arg}')
+    print(f'Total arguments: {len(arguments) + 1}')
+
+if __name__ == '__main__':
+    command()
