023cd6beb7f28490c89f184bdecf1202e9f20729
Lucas Bordanave <lbordana@k0r4p8.42mulhouse.fr>
2026-02-18 16:42:59 +0100

    feat: added py03/ex5

--- py03/ex5/ft_data_stream.py
+++ py03/ex5/ft_data_stream.py
@@ -6,7 +6,24 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/17 19:54:44 by lbordana        #+#    #+#               #
-#  Updated: 2026/02/17 19:54:46 by lbordana        ###   ########.fr        #
+#  Updated: 2026/02/18 16:38:18 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+from typing import Generator
+
+def process(game_event_number: int) -> None:
+    
+    
+
+
+def data_stream(game_event: list) -> None:
+    for nbr in range(1, len(game_event)):
+        yield 'Event {nbr}: {game_event[nbr]}'
+
+
+if __name__ == '__main__':
+    game_event_number = 1000
+    print('=== Game Data Stream Processor ===\n')
+    print(f'Processing {game_event_number} game events')
+    game_event = 
