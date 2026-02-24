191ecb80f17a63be75d772662f3f25fe88136104
Lucas Bordanave <lbordana@k2r1p10.42mulhouse.fr>
2026-02-24 16:13:51 +0100

    feat: added py05/ex0

---
+++ py05/ex0/stream_processor.py
@@ -0,0 +1,66 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  stream_processor.py                               :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/02/24 14:44:02 by lbordana        #+#    #+#               #
+#  Updated: 2026/02/24 16:04:17 by lbordana        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+from typing import Any, List, Dict, Union, Optional
+from abc import ABC, abstractmethod
+
+
+class DataProcessor():
+    def process(self, data: Any) -> str:
+        pass
+
+    def validate(self, data: Any) -> bool:
+        pass
+
+    def format_output(self, result: str) -> str:
+        pass
+
+
+class NumericProcessor():
+    def process(self, data: Any) -> str:
+        pass
+
+    def validate(self, data: Any) -> bool:
+        pass
+
+    def format_output(self, result: str) -> str:
+        pass
+
+
+class TextProcessor():
+    def process(self, data: Any) -> str:
+        pass
+
+    def validate(self, data: Any) -> bool:
+        pass
+
+    def format_output(self, result: str) -> str:
+        pass
+
+
+class LogProcessor():
+    def process(self, data: Any) -> str:
+        pass
+
+    def validate(self, data: Any) -> bool:
+        pass
+
+    def format_output(self, result: str) -> str:
+        pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
