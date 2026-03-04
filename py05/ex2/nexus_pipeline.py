406b09e8ac0b2873589e8acbe593f3d9363e1901
UnixCodable <lucasbordanave@gmail.com>
2026-03-05 00:36:38 +0100

    feat: added py05/ex2

--- py05/ex2/nexus_pipeline.py
+++ py05/ex2/nexus_pipeline.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/03 12:00:13 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/03 12:36:07 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/04 10:46:01 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -15,51 +15,85 @@ from typing import Any, Union, Protocol
 
 
 class ProcessingPipeline(ABC):
+    def __init__(self, pipeline_id: str):
+        self.id = pipeline_id
+
     @abstractmethod
     def process(self, data: Any) -> Union[str, Any]:
         pass
 
 
 class JSONAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id: str):
+        super().__init__(pipeline_id)
+
     def process(self, data: Any) -> Union[str, Any]:
         pass
 
 
 class CSVAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id: str):
+        super().__init__(pipeline_id)
+
     def process(self, data: Any) -> Union[str, Any]:
         pass
 
 
 class StreamAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id: str):
+        super().__init__(pipeline_id)
+
     def process(self, data: Any) -> Union[str, Any]:
         pass
 
 
 class ProcessingStage(Protocol):
-    pass
+    def process(self, data: Any) -> Any:
+        pass
 
 
 class InputStage():
+    def __init__(self):
+        print('Stage 1: Input validation and parsing')
+
     def process(self, data: Any) -> Any:
         pass
 
 
 class TransformStage():
+    def __init__(self):
+        print('Stage 2: Data transformation and enrichment')
+
     def process(self, data: Any) -> Any:
         pass
 
 
-class OutpusStage():
+class OutputStage():
+    def __init__(self):
+        print('Stage 3: Output formatting and delivery')
+
     def process(self, data: Any) -> Any:
         pass
 
 
 class NexusManager():
-    pass
+    def __init__(self):
+        print('\nInitializing Nexus Manager...')
+        print('Pipeline capacity: 1000 streams/second')
+
+    def process_stages(self, stage: ProcessingStage) -> None:
+        pass
 
 
 def main() -> None:
-    pass
+    print('=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===')
+    manager = NexusManager()
+    print('\nCreating Data Processing Pipeline...')
+    input_stage = InputStage()
+    transform_stage = TransformStage()
+    output_stage = OutputStage()
+    print('\n=== Multi-Format Data Processing ===')
+    print('\nProcessing JSON data through pipeline...')
 
 
 if __name__ == '__main__':
