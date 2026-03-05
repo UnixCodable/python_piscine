d2f69354b6bb3440551568466f14ff11ed866d09
Lucas Bordanave <lbordana@k1r3p1.42mulhouse.fr>
2026-03-05 16:44:47 +0100

    feat: added py05/ex2

--- py05/ex2/nexus_pipeline.py
+++ py05/ex2/nexus_pipeline.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/03 12:00:13 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/05 02:08:55 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/05 15:04:06 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -24,7 +24,16 @@ class InputStage():
         print('Stage 1: Input validation and parsing')
 
     def process(self, data: Any) -> Any:
-        pass
+        if isinstance(data[0], JSONAdapter) is True:
+            if data[1]['sensor'] != 'temp':
+                raise ValueError('sensor value must be temperature (temp)')
+            data[1]['value'] = float(data[1]['value'])
+            if data[1]['value'] > 20000:
+                raise ValueError('value is too high to be processed')
+            if data[1]['unit'] not in ('C', 'F'):
+                raise ValueError('unit must be C (Celsius) or F (Farenheit)')
+            print('Input:', data[1])
+            return data[1]
 
 
 class TransformStage():
@@ -32,6 +41,8 @@ class TransformStage():
         print('Stage 2: Data transformation and enrichment')
 
     def process(self, data: Any) -> Any:
+        if isinstance(data[0], JSONAdapter) is True:
+            data
         pass
 
 
@@ -44,13 +55,16 @@ class OutputStage():
 
 
 class ProcessingPipeline(ABC):
-    def __init__(self, pipeline_id: str, stages_list: List[ProcessingStage]):
+    def __init__(self, pipeline_id: str):
         self.id = pipeline_id
-        self.stages = stages_list
+        self.__stages = []
 
     @abstractmethod
     def process(self, data: Any) -> Union[str, Any]:
         pass
+
+    def add_stage(self, stage: ProcessingStage):
+        self.__stages.append(stage)
 
 
 class JSONAdapter(ProcessingPipeline):
@@ -58,7 +72,14 @@ class JSONAdapter(ProcessingPipeline):
         super().__init__(pipeline_id, stages_list)
 
     def process(self, data: Any) -> Union[str, Any]:
-        pass
+        try:
+            for iteration, stage in enumerate(self.stages):
+                data = stage.process([self, data])
+        except ValueError as err:
+            return (f'Error detected in Stage {iteration + 1}:', err)
+        except KeyError as err:
+            return (f'Error detected in Stage {iteration + 1}:'
+                    f'No {err} key in sensor data')
 
 
 class CSVAdapter(ProcessingPipeline):
@@ -66,7 +87,11 @@ class CSVAdapter(ProcessingPipeline):
         super().__init__(pipeline_id, stages_list)
 
     def process(self, data: Any) -> Union[str, Any]:
-        pass
+        try:
+            for iteration, stage in enumerate(self.stages):
+                data = stage.process([self, data])
+        except ValueError as err:
+            return (f'Error detected in Stage {iteration + 1}:', err)
 
 
 class StreamAdapter(ProcessingPipeline):
@@ -74,7 +99,11 @@ class StreamAdapter(ProcessingPipeline):
         super().__init__(pipeline_id, stages_list)
 
     def process(self, data: Any) -> Union[str, Any]:
-        pass
+        try:
+            for iteration, stage in enumerate(self.stages):
+                data = stage.process([self, data])
+        except ValueError as err:
+            return (f'Error detected in Stage {iteration + 1}:', err)
 
 
 class NexusManager():
@@ -82,8 +111,10 @@ class NexusManager():
         print('\nInitializing Nexus Manager...')
         print('Pipeline capacity: 1000 streams/second')
 
-    def process_pipelines(self, pipelines: List[ProcessingPipeline]) -> None:
-        pass
+    def process_pipelines(self, pipelines_data:
+                          List[ProcessingPipeline, Any]) -> None:
+        for pipeline in pipelines_data:
+            pipeline[0].process(pipeline[1])
 
 
 def main() -> None:
@@ -95,7 +126,9 @@ def main() -> None:
     output_stage = OutputStage()
     print('\n=== Multi-Format Data Processing ===')
     print('\nProcessing JSON data through pipeline...')
-    json_data = {"sensor": "temp", "value": 23.5, "unit": "C"}
+    data = {"sensor": "temp", "value": '50.6', "unit": "C"}
+    json = JSONAdapter('PIPELINE001', [input_stage, transform_stage, output_stage])
+    json.process(data)
 
 
 if __name__ == '__main__':
