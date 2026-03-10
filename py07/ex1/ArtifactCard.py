4b45ea8c1903647fd370d616e8ed36fb4fad5601
UnixCodable <lucasbordanave@gmail.com>
2026-03-10 01:24:19 +0100

    feat: added py07/ex1

--- py07/ex1/ArtifactCard.py
+++ py07/ex1/ArtifactCard.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/08 22:58:04 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/09 02:45:31 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/10 01:21:19 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -22,14 +22,26 @@ class ArtifactCard(Card):
         self.effect = effect
 
     def play(self, game_state: dict) -> dict:
-        if self.is_playable(game_state['available_mana']):
-            game_state['battlefield'].append(self)
+        if self.durability == 0:
+            game_state['battlefield'].pop(game_state['battlefield']
+                                          .index(self))
+            return {'card_played': self.name,
+                    'mana_used': self.cost,
+                    'effect': 'Effect has ended, card has been destroyed'}
+        is_playable = self.is_playable(game_state['available_mana'])
+        if is_playable is True:
+            self.activate_ability()
+            if self not in game_state['battlefield']:
+                game_state['battlefield'].append(self)
             return {'card_played': self.name,
                     'mana_used': self.cost,
                     'effect': self.effect}
         return {'card_played': self.name,
                 'mana_used': 0,
-                'effect': 'Artifact not played'}
+                'effect': 'Unable to play card'}
 
     def activate_ability(self) -> dict:
-        pass
+        if self.durability > 0:
+            self.durability -= 1
+            return {'durability': self.durability,
+                    'status': 'running'}
--- py07/ex1/main.py
+++ py07/ex1/main.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/09 00:23:19 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/09 02:51:53 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/09 21:10:50 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -24,15 +24,29 @@ def main() -> None:
     mana_crystal = ArtifactCard('Mana crystal', 2, 'rare', 1,
                                 'Permanent: +1 mana per turn')
     fire_dragon = CreatureCard('Fire Dragon', 5, 'Legendary', 7, 5)
+
     deck.add_card(lightning_bolt)
     deck.add_card(mana_crystal)
     deck.add_card(fire_dragon)
     print('Deck stats:', deck.get_deck_stats())
     deck.shuffle()
-    drew_1 = deck.draw_card()
-    print('Drew:', drew_1.name, f'({drew_1.type})')
-    print('Play result:', drew_1.play({'available_mana': 6,
-                                       'battlefield': []}))
+
+    drew = deck.draw_card()
+    print('\nDrawing and playing cards:')
+
+    print('\nDrew:', drew.name, f'({drew.type})')
+    print('Play result:', drew.play({'available_mana': 6,
+                                     'battlefield': []}))
+    drew = deck.draw_card()
+    print('\nDrew:', drew.name, f'({drew.type})')
+    print('Play result:', drew.play({'available_mana': 6,
+                                     'battlefield': []}))
+    drew = deck.draw_card()
+    print('\nDrew:', drew.name, f'({drew.type})')
+    print('Play result:', drew.play({'available_mana': 6,
+                                     'battlefield': []}))
+    print('')
+    print('Polymorphism in action: Same interface, different card behaviors!')
 
 
 if __name__ == '__main__':
