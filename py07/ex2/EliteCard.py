3cc752f80466d32576cc9976f9c03f7daef11583
UnixCodable <lucasbordanave@gmail.com>
2026-03-11 02:41:43 +0100

    feat: added py07/ex2

--- py07/ex2/EliteCard.py
+++ py07/ex2/EliteCard.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/10 01:25:32 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/10 11:41:41 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/11 02:38:23 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -16,30 +16,58 @@ from ex0.Card import Card
 
 
 class EliteCard(Card, Combatable, Magical):
-    def __init__(self, name: str, cost: int, rarity: str) -> None:
-        super().__init__(self, name, cost, rarity)
+    def __init__(self, name: str, cost: int, rarity: str, defense: int,
+                 attack: int, health: int, combat_type: str) -> None:
+        super().__init__(name, cost, rarity)
+        self.__attack = attack
+        self.__defense = defense
+        self.__health = health
+        self.__combat_type = combat_type
+        self.__count_attack = 0
+        self.__count_block = 0
+        self.__count_spell = 0
+        self.__count_channeling = 0
 
     def cast_spell(self, spell_name: str, targets: list) -> dict:
+        self.__count_spell += 1
         return {'caster': self.name,
                 'spell': spell_name,
                 'targets': targets,
                 'mana_used': 4}
 
     def channel_mana(self, amount: int) -> dict:
+        self.__count_channeling += 1
         return {'channeled': amount,
                 'total_mana': amount + 4}
 
     def get_magic_stats(self) -> dict:
-        pass
+        return {'casted_spells': self.__count_spell,
+                'casted_channeling': self.__count_channeling}
 
     def attack(self, target) -> dict:
-        pass
+        target.__health -= self.__attack
+        return {'attacker': self.name,
+                'target': target.name,
+                'damage': self.__attack,
+                'combat_type': self.__combat_type}
 
     def defend(self, incoming_damage: int) -> dict:
-        pass
+        self.__health -= incoming_damage - self.__defense
+        return {'defender': self.name,
+                'damage_taken': incoming_damage - self.__defense,
+                'damage_blocked': self.__defense,
+                'still_alive': True if self.__health > 0 else False}
 
     def get_combat_stats(self) -> dict:
-        pass
+        return {'number_of_attacks': self.__count_attack,
+                'number_of_blocks': self.__count_block}
 
     def play(self, game_state: dict) -> dict:
-        pass
+        if self.is_playable(game_state['available_mana']):
+            game_state['battlefield'].append(self)
+            return {'card_played': self.name,
+                    'mana_used': self.cost,
+                    'effect': 'Creature summoned to battlefield'}
+        return {'card_played': self.name,
+                'mana_used': 0,
+                'effect': 'Creature not summoned'}
--- py07/ex2/main.py
+++ py07/ex2/main.py
@@ -6,13 +6,29 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/10 01:25:26 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/10 09:04:18 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/11 02:41:32 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
-def main() -> None:
-    pass
+from .EliteCard import EliteCard
 
+
+def main() -> None:
+    elite = EliteCard('Arcane Warrior', 5, 'Legendary', 3, 5, 10, 'melee')
+    ennemy = EliteCard('Enemy', 5, 'Legendary', 3, 5, 10, 'melee')
+    print('\n=== DataDeck Ability System ===')
+    print('\nEliteCard capabilities:')
+    print("- Card: ['play', 'get_card_info', 'is_playable']")
+    print("- Combatable: ['attack', 'defend', 'get_combat_stats']")
+    print("- Magical: ['cast_spell', 'channel_mana', 'get_magic_stats']")
+    print('\nPlaying Arcane Warrior (Elite Card):')
+    print('\nCombat phase:')
+    print('Attack result:', elite.attack(ennemy))
+    print('Defense result:', elite.defend(5))
+    print('\nMagic phase:')
+    print('Spell cast:', elite.cast_spell('Fireball', ['Ennemy1', 'Ennemy2']))
+    print('Mana channel:', elite.channel_mana(3))
+    print('\nMultiple interface implementation successful!')
 
 if __name__ == '__main__':
     main()
