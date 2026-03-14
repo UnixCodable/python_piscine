fcf2097ad56c53cc7118a0c3eed2fa8243c13c08
UnixCodable <lucasbordanave@gmail.com>
2026-03-14 02:37:06 +0100

    feat: added py07/ex3

--- py07/ex3/AgressiveStrategy.py
+++ py07/ex3/AgressiveStrategy.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/11 11:57:36 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/13 15:44:12 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/14 02:32:45 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -23,15 +23,22 @@ class AgressiveStrategy(GameStrategy):
             if card.cost <= available_mana:
                 card.play({'available_mana': available_mana,
                            'battlefield': list(battlefield[1])})
+                battlefield[1].append(card)
                 available_mana -= card.cost
         available_targets = self.prioritize_targets(list(battlefield[0]))
-        for creature in creatures:
+        total_damage = 0
+        for creature in battlefield[1]:
             if len(available_targets) != 0:
                 creature.attack_target(available_targets[0])
+                total_damage += creature.attack
                 if available_targets[0].health < 1:
                     available_targets.pop(0)
             else:
                 break
+        return {'cards_played': [creature.name for creature in battlefield[1]],
+                'mana_used': 10 - available_mana,
+                'targets_attacked': [enemy.name for enemy in battlefield[0]],
+                'damage_dealt': total_damage}
 
     def get_strategy_name(self) -> str:
         return 'AgressiveStrategy'
--- py07/ex3/main.py
+++ py07/ex3/main.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/11 11:57:28 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/14 01:38:48 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/14 02:32:11 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -16,6 +16,13 @@ from ex3.FantasyCardFactory import FantasyCardFactory
 from ex3.AgressiveStrategy import AgressiveStrategy
 
 
+class Player():
+    def __init__(self):
+        self.name = 'Enemy Player'
+        self.health = 50
+        self.attack = 0
+
+
 def main() -> None:
     print('\n=== DataDeck Game Engine ===')
     print('\nConfiguring Fantasy Card Game...')
@@ -23,15 +30,18 @@ def main() -> None:
     engine.configure_engine('FantasyCardFactory', 'AgressiveStrategy')
     factory = FantasyCardFactory()
     deck = factory.create_themed_deck(10)
-    hand = [deck['deck'].draw_card() for _ in range(5)]
-    ennemies = [deck['deck'].draw_card() for _ in range(5)]
+    hand = [deck.draw_card() for _ in range(5)]
+    ennemies = [deck.draw_card() for _ in range(5)]
     ennemies = [ennemy for ennemy in ennemies
                 if isinstance(ennemy, CreatureCard)]
     print('Available types:', factory.get_supported_types())
     print('\nSimulating agressive turn...')
     print(f'Hand: [{", ".join([f"{card.name} ({card.cost})" for card in hand])}]')
+    print('\nTurn execution:')
     strategy = AgressiveStrategy()
-    strategy.execute_turn(hand, [ennemies, []])
+    enemy = Player()
+    print('Strategy:', strategy.get_strategy_name())
+    print('Actions:', strategy.execute_turn(hand, [[enemy], []]))
 
 
 if __name__ == '__main__':
