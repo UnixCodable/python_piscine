b0b492f140f3411d8baceadfce92b6cafd26e168
UnixCodable <lucasbordanave@gmail.com>
2026-03-17 12:36:35 +0100

    feat: added py07/ex4

--- py07/ex4/TournamentPlatform.py
+++ py07/ex4/TournamentPlatform.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/15 17:39:45 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/16 12:21:21 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/17 12:35:05 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -18,7 +18,13 @@ class TournamentPlatform():
         self.__registered = []
 
     def register_card(self, card: TournamentCard) -> str:
-        return f'{card.name} (ID: {card.id})'
+        self.__registered.append(card)
+        rank = card.get_rank_info().get('ranking', 0)
+        stats = card.get_tournament_stats()
+        return (f'\n{card.name} (ID: {card.id})\n'
+                '- Interfaces: [Card, Combatable, Rankable]\n'
+                f'- Rating: {rank}\n'
+                f'- Record: {stats.get('wins', 0)}-{stats.get('losses', 0)}')
 
     def create_match(self, card1_id: str, card2_id: str) -> dict:
         pass
--- py07/ex4/main.py
+++ py07/ex4/main.py
@@ -6,20 +6,21 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/15 17:39:21 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/16 09:25:21 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/17 12:34:08 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 from ex4.TournamentCard import TournamentCard
+from ex4.TournamentPlatform import TournamentPlatform
 
 
 def main() -> None:
     print('\n=== DataDeck Tournament Platform ===')
     print('\nRegistering Tournament Card...')
+    tournament = TournamentPlatform()
     fire_dragon = TournamentCard('Fire Dragon', 4, 'Legendary', 7,
                                  10, 'dragon_001')
-    print(f'\n{fire_dragon.name} (ID: {fire_dragon.id}):')
-    print(f'')
+    print(tournament.register_card(fire_dragon))
 
 
 if __name__ == '__main__':
