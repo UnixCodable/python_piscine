937da41690d34fb4c320ecd5c7e26205145f08f9
UnixCodable <lucasbordanave@gmail.com>
2026-03-21 02:36:48 +0100

    feat: added py08/ex1

--- py08/ex1/loading.py
+++ py08/ex1/loading.py
@@ -6,6 +6,11 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/19 01:00:31 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/19 01:00:32 by lbordana        ###   ########.fr        #
+#  Updated: 2026/03/20 15:08:28 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
+
+import numpy
+import pandas
+import requests
+import matplotlib
