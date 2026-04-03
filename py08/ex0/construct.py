1cce80ff6eb8fa12a1ef64c970a2bb2cef7d78fd
UnixCodable <lucasbordanave@gmail.com>
2026-04-04 00:44:50 +0200

    feat: added py08

--- py08/ex0/construct.py
+++ py08/ex0/construct.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/18 07:27:32 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/19 00:46:42 by lbordana        ###   ########.fr        #
+#  Updated: 2026/04/03 22:47:36 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -26,9 +26,7 @@ def main() -> None:
         print('\nTo enter the construct, run:')
         print('python -m venv matrix_env')
         print('source matrix_env/bin/activate # On Unix')
-        print('matrix_env')
-        print('Scripts')
-        print('activate # On Windows')
+        print('matrix_env\\Scripts\\activate # On Windows')
         print('\nThen run this program again.')
     else:
         print("\nMATRIX STATUS: Welcome to the construct")
--- py08/ex1/loading.py
+++ py08/ex1/loading.py
@@ -6,11 +6,24 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/03/19 01:00:31 by lbordana        #+#    #+#               #
-#  Updated: 2026/03/20 15:08:28 by lbordana        ###   ########.fr        #
+#  Updated: 2026/04/04 00:25:02 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
-import numpy
-import pandas
-import requests
-import matplotlib
+print("LOADING STATUS: Loading programs\n")
+
+try:
+    import pandas
+    print(f"[OK] pandas ({pandas.__version__}) - Data manipulation ready")
+    import numpy as np
+    print(f"[OK] numpy ({np.__version__}) - Numerical computation ready")
+    import requests
+    print(f"[OK] requests ({requests.__version__}) - Network access ready")
+    import matplotlib
+    print(f"[OK] matplotlib ({matplotlib.__version__}) - Visualization ready")
+except ModuleNotFoundError as err:
+    print(f"Error while loading the module : {err}")
+    print("Please check dependencies in requirements.txt")
+    print("Use 'pip install -r requirements.txt'")
+    print("Or 'poetry install' then 'poetry run python loading.py'")
+
