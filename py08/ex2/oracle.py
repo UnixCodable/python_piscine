4d648e67e0b6b2831c7b9a82b1e2fec2c0636e42
UnixCodable <lucasbordanave@gmail.com>
2026-04-17 10:52:42 +0200

    feat: added py08/ex2

--- py08/ex2/oracle.py
+++ py08/ex2/oracle.py
@@ -1,14 +1,14 @@
-# ************************************************************************* #
-#                                                                           #
-#                                                      :::      ::::::::    #
-#  oracle.py                                         :+:      :+:    :+:    #
-#                                                  +:+ +:+         +:+      #
-#  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
-#                                              +#+#+#+#+#+   +#+            #
-#  Created: 2026/04/16 11:46:53 by lbordana        #+#    #+#               #
-#  Updated: 2026/04/16 15:04:44 by lbordana        ###   ########.fr        #
-#                                                                           #
-# ************************************************************************* #
+# *************************************************************************** #
+#                                                                             #
+#                                                         :::      ::::::::   #
+#   oracle.py                                           :+:      :+:    :+:   #
+#                                                     +:+ +:+         +:+     #
+#   By: lbordana <lbordana@student.42mulhouse.fr>   +#+  +:+       +#+        #
+#                                                 +#+#+#+#+#+   +#+           #
+#   Created: 2026/04/16 11:46:53 by lbordana           #+#    #+#             #
+#   Updated: 2026/04/17 10:50:57 by lbordana          ###   ########.fr       #
+#                                                                             #
+# *************************************************************************** #
 
 from dotenv import load_dotenv
 import os
@@ -18,7 +18,7 @@ def read_matrix():
     print('\nORACLE STATUS: Reading the Matrix...\n')
     load_dotenv()
     print('Configuration loaded:')
-    print('Mode:', os.environ.get('MATRIX_MODE', 'No data loaded'))
+    print('Mode:', os.getenv('MATRIX_MODE', 'Warning: No data loaded'))
 
 
 if __name__ == '__main__':
