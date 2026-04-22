25a005b0345af19a3faa2137cc708db0fe73faf5
UnixCodable <lucasbordanave@gmail.com>
2026-04-22 11:42:19 +0200

    feat: added py09/ex0

--- py09/space_station.py
+++ py09/space_station.py
@@ -6,7 +6,7 @@
 #   By: lbordana <lbordana@student.42mulhouse.fr>   +#+  +:+       +#+        #
 #                                                 +#+#+#+#+#+   +#+           #
 #   Created: 2026/04/20 16:13:27 by lbordana           #+#    #+#             #
-#   Updated: 2026/04/20 16:19:57 by lbordana          ###   ########.fr       #
+#   Updated: 2026/04/20 16:46:48 by lbordana          ###   ########.fr       #
 #                                                                             #
 # *************************************************************************** #
 
@@ -15,3 +15,10 @@ from pydantic import BaseModel, Field
 
 class SpaceStation(BaseModel):
     station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: DateTime field
+    is_operational: Boolean, defaults to True
+    notes: Optional string, max 200 characters
