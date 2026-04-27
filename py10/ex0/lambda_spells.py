7ce4c48054bab9e0079189f5eaf953275fde4364
Lucas Bordanave <lbordana@k1r3p8.42mulhouse.fr>
2026-04-27 19:24:45 +0200

    feat: added py10

--- py10/ex0/lambda_spells.py
+++ py10/ex0/lambda_spells.py
@@ -6,7 +6,7 @@
 #  By: lbordana <lbordana@student.42mulhouse.f   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/25 00:22:46 by lbordana        #+#    #+#               #
-#  Updated: 2026/04/25 02:43:54 by lbordana        ###   ########.fr        #
+#  Updated: 2026/04/27 19:24:12 by lbordana        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -32,17 +32,37 @@ def mage_stats(mages: list[dict]) -> dict:
 
 
 def main() -> None:
-    # print(artifact_sorter(
-    #     [{'name': 'Alphonse', 'power': 6, 'type': 'Fire'},
-    #      {'name': 'John', 'power': 3, 'type': 'Water'},
-    #      {'name': 'Alex', 'power': 9, 'type': 'Electric'},]
-    #     ))
-    # print(power_filter(
-    #     [{'name': 'Alphonse', 'power': 6, 'type': 'Fire'},
-    #      {'name': 'John', 'power': 3, 'type': 'Water'},
-    #      {'name': 'Alex', 'power': 9, 'type': 'Electric'},], 5
-    #     ))
-    # print(' '.join(spell_transformer(['fireball', 'heal', 'shield'])))
+
+    # Checking artifact sorter
+
+    print('\nTesting artifact sorter...')
+    sorter = artifact_sorter(
+        [{'name': 'Staff', 'power': 6, 'type': 'Fire'},
+         {'name': 'Orb', 'power': 3, 'type': 'Cristal'},
+         {'name': 'Sword', 'power': 9, 'type': 'Frozen'},]
+        )
+    print(f"{sorter[0].get('type')} {sorter[0].get('name')} "
+          f"({sorter[0].get('power')} power) comes "
+          f"before {sorter[1].get('type')} {sorter[1].get('name')} "
+          f"({sorter[1].get('power')} power)")
+
+    # Checking power filter
+
+    print('\nTesting power filter...')
+    print(power_filter(
+        [{'name': 'Alphonse', 'power': 6, 'type': 'Fire'},
+         {'name': 'John', 'power': 3, 'type': 'Water'},
+         {'name': 'Alex', 'power': 9, 'type': 'Electric'},], 5
+        ))
+
+    # Checking spell transformer
+
+    print('\nTesting spell transformer...')
+    print(' '.join(spell_transformer(['fireball', 'heal', 'shield'])))
+
+    # Checking mage stats
+
+    print('\nTesting mage stats...')
     print(mage_stats(
         [{'name': 'Alphonse', 'power': 6, 'type': 'Fire'},
          {'name': 'John', 'power': 3, 'type': 'Water'},
